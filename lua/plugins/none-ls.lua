eda9ac3c26a693ae7b21b6769a0c76b6484d3639
Nick Le Large <nick.lelarge@kit.edu>
2024-04-08 10:01:41 +0200

    Add mason-null-ls to autoinstall formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,22 @@
 return {
-	"nvimtools/none-ls.nvim",
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
 	config = function()
-		local null_ls = require("null-ls")
+		require("mason").setup()
+
 		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.stylua,
 			},
+			-- Auto-format on save
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
@@ -21,6 +30,12 @@ return {
 				end
 			end,
 		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "Format file" })
+
+		require("mason-null-ls").setup({
+			ensure_installed = nil,
+			automatic_installation = true,
+		})
+
+		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format file" })
 	end,
 }
