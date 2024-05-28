d71685fea2389754eced5ccd636bbf61dc31406c
Nick Le Large <nick.lelarge@kit.edu>
2024-05-28 09:26:46 +0200

    Enable relative line numbers

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Un-comment to use system clipboard as default
 -- vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
