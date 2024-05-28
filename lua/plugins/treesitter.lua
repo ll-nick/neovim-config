80507b810848646e114b58ee9c7873a3b735a73c
Nick Le Large <nick.lelarge@kit.edu>
2024-05-28 09:27:05 +0200

    Remove treesitter parsers that are currently not being installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			local config = require("nvim-treesitter.configs")
 			config.setup({
-				ensure_installed = { "c", "cpp", "python", "lua", "vim", "vimdoc", "javascript", "html" },
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
 				auto_install = true,
 				highlight = { enable = true },
 				indent = { enable = true },
