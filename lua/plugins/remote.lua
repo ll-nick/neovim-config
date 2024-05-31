65d6393544112076344b02277fdf0bc5fd846553
Nick Le Large <nick.lelarge@kit.edu>
2024-05-31 13:58:33 +0200

    Add remote-nvim

---
+++ lua/plugins/remote.lua
@@ -0,0 +1,23 @@
+return {
+	"amitds1997/remote-nvim.nvim",
+	version = "*", -- Pin to GitHub releases
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- For standard functions
+		"MunifTanjim/nui.nvim", -- To build the plugin UI
+		"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+	},
+	config = function()
+		require("remote-nvim").setup({
+			client_callback = function(port, _)
+				local cmd = ("tmux new-window -n 'nvim - remote' 'nvim --server localhost:%s --remote-ui'"):format(port)
+				local result = vim.fn.system(cmd)
+				if vim.v.shell_error ~= 0 then
+					vim.notify(
+						("Failed to open tmux window with exit code %s: %s"):format(vim.v.shell_error, result),
+						vim.log.levels.ERROR
+					)
+				end
+			end,
+		})
+	end,
+}
