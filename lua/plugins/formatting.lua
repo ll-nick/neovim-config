56d552ca417f6610048514f282142d0d234e6f6b
Nick Le Large <nick.lelarge@kit.edu>
2024-06-13 09:12:56 +0200

    Merge whitespace into formatting.lua

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,42 +1,57 @@
 return {
-	"jay-babu/mason-null-ls.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"williamboman/mason.nvim",
-		"nvimtools/none-ls.nvim",
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason").setup()
+
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.clang_format,
+					null_ls.builtins.formatting.black,
+					null_ls.builtins.formatting.stylua,
+				},
+				-- Auto-format on save
+				on_attach = function(client, bufnr)
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
+
+			require("mason-null-ls").setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
+		end,
+		keys = {
+			{ "<leader>cf", vim.lsp.buf.format, desc = "Format buffer" },
+		},
 	},
-	config = function()
-		require("mason").setup()
+	{
+		"johnfrankmorgan/whitespace.nvim",
 
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.stylua,
+		keys = {
+			{
+				"<Leader>tw",
+				function()
+					require("whitespace-nvim").trim()
+				end,
+				desc = "Trim whitespace",
 			},
-			-- Auto-format on save
-			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
-		})
-
-		require("mason-null-ls").setup({
-			ensure_installed = nil,
-			automatic_installation = true,
-		})
-	end,
-	keys = {
-		{ "<leader>cf", vim.lsp.buf.format, desc = "Format buffer" },
+		},
 	},
 }
