257fd086f988c41cdf0fe909c978dc4c8cadfdec
Nick Le Large <nick.lelarge@kit.edu>
2024-07-08 08:51:27 +0200

    Add some convenience mappings for yanking and pasting

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,12 +1,21 @@
+vim.g.mapleader = " "
+
+-- Set indentation
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- Set line numbers
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- Un-comment to use system clipboard as default
--- vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+-- Allow pasting without overwriting the yank register
+vim.keymap.set("v", "<leader>p", [["_dP]])
 
-vim.g.mapleader = " "
+-- Yank to system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- Highlight yanked text
+vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })")
