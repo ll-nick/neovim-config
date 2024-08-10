5ff56b0bb2a3151ed8f202088832b973212dfa96
Nick Le Large <github@le-large.net>
2024-08-10 21:49:03 +0200

    Add cmp-path and cmp-buffer

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -16,6 +16,8 @@ return {
 		dependencies = {
 			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
@@ -44,7 +46,7 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
-				}, {
+					{ name = "path" },
 					{ name = "buffer" },
 				}),
 			})
