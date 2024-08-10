14aaccde302ee9aa6f9e5f151bfea264addf932a
Nick Le Large <github@le-large.net>
2024-08-10 21:48:44 +0200

    Reorder completion plugins

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,17 +12,14 @@ return {
 		opts = {},
 	},
 	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
+		"hrsh7th/nvim-cmp",
 		dependencies = {
+			"onsails/lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
