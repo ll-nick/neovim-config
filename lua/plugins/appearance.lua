39a6944afc127e2a284687396eacb5d7dc7fb262
Nick Le Large <nick.lelarge@kit.edu>
2024-11-04 10:16:34 +0100

    Tweak lua line a bit to have rounded of separators

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -40,10 +40,12 @@ return {
 	},
 	{
 		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("lualine").setup({
 				options = {
-					theme = "catppuccin",
+					component_separators = { left = "|", right = "|" },
+					section_separators = { left = "◗", right = "◖" },
 				},
 			})
 		end,
