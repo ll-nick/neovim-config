64c2ab10259e4f36373b861bc652fbf956b9d575
Nick Le Large <nick.lelarge@kit.edu>
2024-12-06 10:59:16 +0100

    Break keymaps out of vim-options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,31 +10,9 @@ vim.cmd("set shiftwidth=4")
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- Allow pasting without overwriting the yank register
-vim.keymap.set("v", "<leader>p", [["_dP]])
-
--- Yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- Highlight yanked text
-vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })")
-
--- Save as sudo
-vim.api.nvim_set_keymap("c", "w!!", "w !sudo tee > /dev/null %", { noremap = true, silent = true })
-
 -- Enable spell checking
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
 
--- Create new split
-vim.keymap.set("n", "<leader>sh", ":split<CR>", {})
-vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", {})
-
--- Zoom in/out by moving pane to new tab
-vim.keymap.set("n", "<leader>zi", ":tab split<CR>", {})
-vim.keymap.set("n", "<leader>zo", ":tab close<CR>", {})
-
--- Center of half page scroll_docs
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Highlight yanked text
+vim.cmd("au TextYankPost * silent! lua vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 })")
