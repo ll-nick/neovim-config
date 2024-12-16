f669971e8c26626210a695fc0bb0c93c87ffcc06
Nick Le Large <nick.lelarge@kit.edu>
2024-12-16 07:22:16 +0100

    Tidy up keybinds by binding set command to local variable

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,21 +1,22 @@
+local set = vim.keymap.set
 -- Allow pasting without overwriting the yank register
-vim.keymap.set("v", "<leader>p", [["_dP]])
+set("v", "<leader>p", [["_dP]])
 
 -- Yank to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
+set({ "n", "v" }, "<leader>y", [["+y]])
+set("n", "<leader>Y", [["+Y]])
 
 -- Save as sudo
-vim.keymap.set("c", "w!!", "w !sudo tee > /dev/null %", { silent = false })
+set("c", "w!!", "w !sudo tee > /dev/null %", { silent = false })
 
 -- Create new split
-vim.keymap.set("n", "<leader>sh", ":split<CR>", {})
-vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", {})
+set("n", "<leader>sh", ":split<CR>", {})
+set("n", "<leader>sv", ":vsplit<CR>", {})
 
 -- Zoom in/out by moving pane to new tab
-vim.keymap.set("n", "<leader>zi", ":tab split<CR>", {})
-vim.keymap.set("n", "<leader>zo", ":tab close<CR>", {})
+set("n", "<leader>zi", ":tab split<CR>", {})
+set("n", "<leader>zo", ":tab close<CR>", {})
 
 -- Center of half page scroll_docs
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+set("n", "<C-d>", "<C-d>zz")
+set("n", "<C-u>", "<C-u>zz")
