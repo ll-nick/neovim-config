a322a454b7fb6706388e4591f356df32cbf592de
Nick Le Large <github@le-large.net>
2025-01-10 23:41:30 +0100

    Add some kickstart-inspired vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,26 @@ vim.g.mapleader = " "
 vim.wo.number = true
 vim.wo.relativenumber = true
 
+-- The mode is already displayed in the status line
+vim.opt.showmode = false
+
+-- Wrapped lines keep indentation
+vim.opt.breakindent = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Prevent jumping due to sign column turning on and off
+vim.opt.signcolumn = "yes"
+
+-- Show whitespace characters
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 5
+
 -- Enable spell checking
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
