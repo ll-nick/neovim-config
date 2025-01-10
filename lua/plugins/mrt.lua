66201e708580becc107bfb8910247286f9fccdde
Nick Le Large <nick.lelarge@kit.edu>
2025-01-10 10:26:48 +0100

    Add keybind for mrt package picker

--- lua/plugins/mrt.lua
+++ lua/plugins/mrt.lua
@@ -1,6 +1,9 @@
 return {
   "ll-nick/mrt.nvim",
-
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+  },
   config = function()
     require("mrt").setup({
       pane_handler = "tmux",
@@ -10,6 +13,7 @@ return {
     { "<leader>bw", "<cmd>MrtBuildWorkspace<cr>", desc = "Build workspace" },
     { "<leader>bp", "<cmd>MrtBuildCurrentPackage<cr>", desc = "Build current package" },
     { "<leader>bt", "<cmd>MrtBuildCurrentPackageTests<cr>", desc = "Build tests for current package" },
+    { "<leader>fp", "<cmd>MrtPickCatkinPackage<cr>", desc = "Pick catkin package" },
     { "<leader>sp", "<cmd>MrtSwitchCatkinProfile<cr>", desc = "Switch catkin profile" },
   },
 }
