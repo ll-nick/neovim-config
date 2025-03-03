efc57d409f84826c8d8b25269a1b843521565b9c
Nick Le Large <nick.lelarge@kit.edu>
2025-03-03 09:33:31 +0100

    Add a keybind to dismiss notifications

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -33,5 +33,9 @@ return {
     config = function()
       vim.notify = require("notify")
     end,
+    keys = {
+      { "<Esc>", "<cmd>lua require('notify').dismiss()<cr>", "n", desc = "Dismiss notification" },
+    },
+    lazy = false,
   },
 }
