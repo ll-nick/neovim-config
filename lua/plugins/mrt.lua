5e338a893294bcc1d63bab47356f654bac5f8157
Nick Le Large <nick.lelarge@kit.edu>
2025-03-06 08:35:56 +0100

    Adjust mrt.nvim config to upstream changes

--- lua/plugins/mrt.lua
+++ lua/plugins/mrt.lua
@@ -1,13 +1,44 @@
 return {
   "ll-nick/mrt.nvim",
+
   dependencies = {
     { "nvim-lua/plenary.nvim" },
     { "nvim-telescope/telescope.nvim" },
+    {
+      "stevearc/overseer.nvim",
+      opts = {
+        task_list = {
+          max_width = { 100, 0.5 },
+          min_width = { 60, 0.4 },
+          max_height = { 40, 0.5 },
+          min_height = { 20, 0.3 },
+          bindings = {
+            ["<leader>qq"] = "OpenQuickFix",
+            ["L"] = "IncreaseDetail",
+            ["H"] = "DecreaseDetail",
+            ["<C-l>"] = false,
+            ["<C-h>"] = false,
+            ["<C-L>"] = false,
+            ["<C-H>"] = false,
+            ["<C-u>"] = "ScrollOutputUp",
+            ["<C-d>"] = "ScrollOutputDown",
+          },
+        },
+      },
+      keys = {
+        { "<leader>oo", "<cmd>OverseerToggle<cr>", desc = "Toggle overseer" },
+      },
+    },
   },
   config = function()
-    require("mrt").setup({
-      pane_handler = "tmux",
-    })
+    require("mrt.overseer").register_build_template(
+      "MRT Build: Simulation",
+      { "build", "-j4", "--release", "simulation_adenauerring" }
+    )
+    require("mrt.overseer").register_build_template(
+      "MRT Build: Behavior Node",
+      { "build", "-j4", "behavior_arbitration_ros_tool" }
+    )
   end,
   keys = {
     { "<leader>bw", "<cmd>MrtBuildWorkspace<cr>", desc = "Build workspace" },
@@ -15,5 +46,19 @@ return {
     { "<leader>bt", "<cmd>MrtBuildCurrentPackageTests<cr>", desc = "Build tests for current package" },
     { "<leader>fp", "<cmd>MrtPickCatkinPackage<cr>", desc = "Pick catkin package" },
     { "<leader>sp", "<cmd>MrtSwitchCatkinProfile<cr>", desc = "Switch catkin profile" },
+    {
+      "<leader>bs",
+      function()
+        require("overseer").run_template({ name = "MRT Build: Simulation" })
+      end,
+      desc = "Build simulation",
+    },
+    {
+      "<leader>bb",
+      function()
+        require("overseer").run_template({ name = "MRT Build: Behavior Node" })
+      end,
+      desc = "Build behavior node",
+    },
   },
 }
