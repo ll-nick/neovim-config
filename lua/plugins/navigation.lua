05906335042cc240b2380ab2f1a7f118f3d90557
Nick Le Large <nick.lelarge@kit.edu>
2025-03-07 09:27:37 +0100

    Dimm search area in leap.nvim

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -64,6 +64,7 @@ return {
     dependencies = { "tpope/vim-repeat" },
     config = function()
       require("leap").create_default_mappings()
+      vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
     end,
   },
   -- Oil
