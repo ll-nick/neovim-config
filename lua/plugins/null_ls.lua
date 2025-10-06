6e4d2887ac073199e62996be839b6fc0cf9ef451
ll-nick <github@le-large.com>
2025-10-06 20:23:18 +0200

    Add nix-related setup
    We only need the formatter on a nix-based system where the packages will
    not have to be handled by mason

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,15 +1,22 @@
+local host_capabilities = require("host_capabilities")
+
 return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.shfmt.with({
-          extra_args = { "--indent", "4", "--case-indent", "--space-redirects" },
-        }),
-      },
-    })
+
+    local sources = {
+      null_ls.builtins.formatting.clang_format,
+      null_ls.builtins.formatting.stylua,
+      null_ls.builtins.formatting.shfmt.with({
+        extra_args = { "--indent", "4", "--case-indent", "--space-redirects" },
+      }),
+    }
+
+    if host_capabilities.has_executable("nix") then
+      table.insert(sources, null_ls.builtins.formatting.alejandra)
+    end
+
+    null_ls.setup({ sources = sources })
   end,
 }
