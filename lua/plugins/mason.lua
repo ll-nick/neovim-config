1d4e3a8d71b72b8c2b33eb75941c684ce31f6423
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-10-17 10:29:45 +0200

    Revert "Add nix-related setup"
    This reverts commit 6e4d2887ac073199e62996be839b6fc0cf9ef451.
    Without home manager, this is still relevant

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,11 +1,8 @@
-local host_capabilities = require("host_capabilities")
-
 return {
   "owallb/mason-auto-install.nvim",
   dependencies = {
     { "williamboman/mason.nvim", opts = {} },
   },
-  enabled = not host_capabilities.has_executable("nix"),
   opts = {
     packages = {
       "basedpyright",
--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -1,22 +1,15 @@
-local host_capabilities = require("host_capabilities")
-
 return {
   "nvimtools/none-ls.nvim",
   config = function()
     local null_ls = require("null-ls")
-
-    local sources = {
-      null_ls.builtins.formatting.clang_format,
-      null_ls.builtins.formatting.stylua,
-      null_ls.builtins.formatting.shfmt.with({
-        extra_args = { "--indent", "4", "--case-indent", "--space-redirects" },
-      }),
-    }
-
-    if host_capabilities.has_executable("nix") then
-      table.insert(sources, null_ls.builtins.formatting.alejandra)
-    end
-
-    null_ls.setup({ sources = sources })
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.shfmt.with({
+          extra_args = { "--indent", "4", "--case-indent", "--space-redirects" },
+        }),
+      },
+    })
   end,
 }
