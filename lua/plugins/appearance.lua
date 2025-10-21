2c3053e5de808b466e0dae26132d84a71b230bdc
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-10-21 10:55:39 +0200

    Initialize colorscheme to mocha before enabling auto-dark-mode

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -7,14 +7,19 @@ return {
       name = "catppuccin",
       priority = 1000,
     },
-    opts = {
-      set_dark_mode = function()
-        vim.cmd.colorscheme("catppuccin-mocha")
-      end,
-      set_light_mode = function()
-        vim.cmd.colorscheme("catppuccin-latte")
-      end,
-    },
+    config = function()
+      local opts = {
+        set_dark_mode = function()
+          vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+        set_light_mode = function()
+          vim.cmd.colorscheme("catppuccin-latte")
+        end,
+        fallback = "dark",
+      }
+      vim.cmd.colorscheme("catppuccin-mocha")
+      require("auto-dark-mode").setup(opts)
+    end,
   },
   {
     "nvim-lualine/lualine.nvim",
