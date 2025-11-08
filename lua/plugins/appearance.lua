cc538b7f906fc5d97248793a3bd80fbe0ebea646
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-11-09 00:19:51 +0100

    Add tpipeline

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -22,10 +22,12 @@ return {
     end,
   },
   {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
+    "vimpostor/vim-tpipeline",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      opts = {
         options = {
           component_separators = { left = "|", right = "|" },
           section_separators = { left = "◗", right = "◖" },
@@ -33,12 +35,32 @@ return {
         sections = {
           lualine_a = { "mode" },
           lualine_b = { "branch", "diff", "diagnostics" },
-          lualine_c = { "filename" },
+          lualine_c = { { "filename", color = { bg = "NONE" } } },
           lualine_x = { "" },
           lualine_y = { "" },
           lualine_z = { "" },
         },
-      })
+      },
+      config = function(_, opts)
+        require("lualine").setup(opts)
+
+        -- https://github.com/vimpostor/vim-tpipeline/issues/53
+        if vim.env.TMUX then
+          vim.api.nvim_create_autocmd({ "FocusGained", "ColorScheme" }, {
+            callback = function()
+              vim.defer_fn(function()
+                vim.opt.laststatus = 0
+              end, 100)
+            end,
+          })
+
+          vim.o.laststatus = 0
+        end
+      end,
+      event = "VimEnter",
+    },
+    init = function()
+      vim.g.tpipeline_autoembed = 0
     end,
   },
   {
