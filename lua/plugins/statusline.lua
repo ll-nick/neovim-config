10701f983310cc2e81b7adcdbb82dd0eaa2889ad
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-11-09 10:13:25 +0100

    Add macro recording hint to statusline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,3 +1,43 @@
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return ""
+  else
+    return "Recording @" .. recording_register .. " │"
+  end
+end
+
+local function create_recording_autocmd()
+  vim.api.nvim_create_autocmd("RecordingEnter", {
+    callback = function()
+      require("lualine").refresh({
+        place = { "statusline" },
+      })
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("RecordingLeave", {
+    callback = function()
+      -- This is going to seem really weird!
+      -- Instead of just calling refresh we need to wait a moment because of the nature of
+      -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+      -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+      -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+      -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+      local timer = vim.loop.new_timer()
+      timer:start(
+        50,
+        0,
+        vim.schedule_wrap(function()
+          require("lualine").refresh({
+            place = { "statusline" },
+          })
+        end)
+      )
+    end,
+  })
+end
+
 return {
   "vimpostor/vim-tpipeline",
   event = "VeryLazy",
@@ -12,14 +52,25 @@ return {
       sections = {
         lualine_a = { "mode" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", color = { bg = "NONE" } } },
-        lualine_x = { "" },
+        lualine_c = { {
+          "filename",
+          color = { bg = "NONE" },
+        } },
+        lualine_x = {
+          {
+            "macro-recording",
+            fmt = show_macro_recording,
+            color = "NonText",
+          },
+        },
         lualine_y = { "" },
         lualine_z = { "" },
       },
     },
     config = function(_, opts)
       require("lualine").setup(opts)
+
+      create_recording_autocmd()
 
       -- https://github.com/vimpostor/vim-tpipeline/issues/53
       if vim.env.TMUX then
