7f43d874db08cadd4c18649c163495c50a215cb5
ll-nick <68419636+ll-nick@users.noreply.github.com>
2025-11-17 13:08:59 +0100

    Simplify Lazygit keybind

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,21 +3,13 @@ return {
   opts = {
     lazygit = {},
   },
-  config = function()
-    local function open_lazygit_for_current_file()
-      local git_root = vim.fn.systemlist("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse --show-toplevel")[1]
-      if vim.v.shell_error ~= 0 then
-        print("Not inside a Git repository")
-        return
-      end
-
-      -- Open lazygit
-      require("snacks.lazygit").open({ cwd = git_root })
-    end
-
-    vim.api.nvim_create_user_command("LazyGitCurrentFile", open_lazygit_for_current_file, {})
-  end,
   keys = {
-    { "<leader>gg", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit" },
+    {
+      "<leader>gg",
+      function()
+        Snacks.lazygit.open({ cwd = Snacks.git.get_root() })
+      end,
+      desc = "Open LazyGit for Current File",
+    },
   },
 }
