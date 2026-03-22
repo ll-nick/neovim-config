2028871d8439762b6da5a90bead1109f7aa764f2
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-03-22 12:02:44 +0100

    Markdown preview now works on remote

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,12 +1,12 @@
-local host_capabilities = require("host_capabilities")
-
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  enabled = host_capabilities.has_display,
   build = function()
     vim.fn["mkdp#util#install"]()
+  end,
+  init = function()
+    vim.g.mkdp_open_to_the_world = 1
   end,
   keys = {
     { "<leader>mp", "<Cmd>MarkdownPreview<CR>", desc = "Preview Markdown" },
