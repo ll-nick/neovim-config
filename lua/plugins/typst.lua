f4cb03a6da6c9be0190bf896c7a518663f69b4e2
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-03-24 12:42:51 +0100

    Auto-fill quickfix list with typst diagnostics

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -8,6 +8,22 @@ return {
   opts = {
     dependencies_bin = { ["tinymist"] = "tinymist" },
   },
+  config = function(_, opts)
+    require("typst-preview").setup(opts)
+    vim.api.nvim_create_autocmd("DiagnosticChanged", {
+      callback = function(args)
+        if not vim.api.nvim_buf_is_valid(args.buf) then
+          return
+        end
+        if vim.bo[args.buf].filetype ~= "typst" then
+          return
+        end
+        vim.schedule(function()
+          vim.diagnostic.setqflist({ open = false })
+        end)
+      end,
+    })
+  end,
   keys = {
     { "<leader>pp", "<cmd>TypstPreviewToggle<cr>", desc = "Toggle Typst preview" },
     { "<leader>ps", "<cmd>TypstPreviewSyncCursor<cr>", desc = "Sync preview to cursor" },
