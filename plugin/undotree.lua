ed5f7decf3c656bcc236ea57c9aad4b0f7e104a1
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-01 21:00:07 +0200

    Replace undotree with built-in plugin

---
+++ plugin/undotree.lua
@@ -0,0 +1,14 @@
+-- Persistent undo configuration
+if vim.fn.has("persistent_undo") == 1 then
+  local target_path = vim.fn.expand("~/.config/nvim/.undodir")
+
+  if vim.fn.isdirectory(target_path) == 0 then
+    vim.fn.mkdir(target_path, "p")
+  end
+
+  vim.opt.undodir = target_path
+  vim.opt.undofile = true
+end
+
+vim.cmd("packadd nvim.undotree")
+vim.keymap.set("n", "<leader>ut", require("undotree").open)
