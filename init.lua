8a0db695990fe4c4d1d99951becf2c0e31eb8dee
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-01 21:47:08 +0200

    Add a loop to source lua files containing vim.pack plugins

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+-- vim.pack plugins
+for _, f in ipairs(vim.fn.glob(vim.fn.stdpath("config") .. "/lua/pack/*.lua", false, true)) do
+  dofile(f)
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
