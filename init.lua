57272b02ee5cc76a1286f3bc007d9ba5ef2f15fc
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-02 16:44:58 +0200

    Move plugin files into plugin/ for autosourcing

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- vim.pack plugins
-for _, f in ipairs(vim.fn.glob(vim.fn.stdpath("config") .. "/lua/pack/*.lua", false, true)) do
-  dofile(f)
-end
