c4ae8c2025e0986c28870e0f8c9a45048b066feb
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-02 16:45:37 +0200

    Enable vimtex, it now works as expected

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -4,9 +4,8 @@ if not (host_capabilities.has_display and host_capabilities.has_executable("late
   return
 end
 
--- TODO: This won't work with lazy.nvim, uncomment after lazy.nvim is removed
--- vim.g.vimtex_view_method = "zathura"
--- vim.g.vimtex_mappings_prefix = "<leader>l"
--- vim.g.vimtex_quickfix_mode = 0
---
--- vim.pack.add({ "https://github.com/lervag/vimtex" })
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_mappings_prefix = "<leader>l"
+vim.g.vimtex_quickfix_mode = 0
+
+vim.pack.add({ "https://github.com/lervag/vimtex" })
