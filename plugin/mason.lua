d729602dc3f9a8ab50d2600f5c7e4745227e727b
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-05 22:49:05 +0200

    Add mason setup call that got lost along the way

--- plugin/mason.lua
+++ plugin/mason.lua
@@ -1,5 +1,6 @@
 vim.pack.add({ "https://github.com/owallb/mason-auto-install.nvim", "https://github.com/williamboman/mason.nvim" })
 
+require("mason").setup()
 require("mason-auto-install").setup({
   packages = {
     "basedpyright",
