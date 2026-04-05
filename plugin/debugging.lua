c6433a6d8b7e62553010868eae4bf21408ea856e
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-05 22:40:06 +0200

    Defer loading debugging and testing setup

--- plugin/debugging.lua
+++ plugin/debugging.lua
@@ -1,27 +1,3 @@
-local function set_conditional_breakpoint()
-  vim.ui.input({ prompt = "Enter condition for breakpoint: " }, function(condition)
-    if condition then
-      require("dap").set_breakpoint(condition)
-    end
-  end)
-end
-
-local function set_hit_breakpoint()
-  vim.ui.input({ prompt = "Enter hit count for breakpoint: " }, function(hit_condition)
-    if hit_condition then
-      require("dap").set_breakpoint(nil, hit_condition)
-    end
-  end)
-end
-
-local function set_log_breakpoint()
-  vim.ui.input({ prompt = "Enter log message for breakpoint: " }, function(log_message)
-    if log_message then
-      require("dap").set_breakpoint(nil, nil, log_message)
-    end
-  end)
-end
-
 vim.pack.add({
   "https://github.com/williamboman/mason.nvim",
   "https://github.com/jay-babu/mason-nvim-dap.nvim",
@@ -31,117 +7,141 @@ vim.pack.add({
   "https://github.com/mfussenegger/nvim-dap",
 })
 
-require("mason").setup()
-require("mason-nvim-dap").setup({
-  ensure_installed = {
-    "codelldb",
-    "python",
-  },
-  handlers = {},
-})
-
-local dap = require("dap")
-local dap_ui = require("dapui")
-local dap_virtual_text = require("nvim-dap-virtual-text")
-dap_ui.setup({
-  layouts = {
-    {
-      -- Remove all the default elements on the left as I use them via floats
-      elements = {},
-      position = "left",
-      size = 40,
+vim.schedule(function()
+  require("mason-nvim-dap").setup({
+    ensure_installed = {
+      "codelldb",
+      "python",
     },
-    {
-      elements = {
-        {
-          id = "repl",
-          size = 0.4,
-        },
-        {
-          id = "console",
-          size = 0.6,
-        },
+    handlers = {},
+  })
+
+  local dap = require("dap")
+  local dap_ui = require("dapui")
+  local dap_virtual_text = require("nvim-dap-virtual-text")
+  dap_ui.setup({
+    layouts = {
+      {
+        -- Remove all the default elements on the left as I use them via floats
+        elements = {},
+        position = "left",
+        size = 40,
       },
-      position = "bottom",
-      size = 10,
+      {
+        elements = {
+          {
+            id = "repl",
+            size = 0.4,
+          },
+          {
+            id = "console",
+            size = 0.6,
+          },
+        },
+        position = "bottom",
+        size = 10,
+      },
     },
-  },
-})
-dap_virtual_text.setup()
+  })
+  dap_virtual_text.setup()
 
-vim.fn.sign_define(
-  "DapBreakpoint",
-  { text = "●", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-  "DapBreakpointCondition",
-  { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-  "DapBreakpointRejected",
-  { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
-)
-vim.fn.sign_define(
-  "DapLogPoint",
-  { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
-)
-vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
+  dap.listeners.before.attach.dapui_config = function()
+    dap_ui.open()
+  end
+  dap.listeners.before.launch.dapui_config = function()
+    dap_ui.open()
+  end
+  dap.listeners.before.event_terminated.dapui_config = function()
+    dap_ui.close()
+  end
+  dap.listeners.before.event_exited.dapui_config = function()
+    dap_ui.close()
+  end
 
-dap.listeners.before.attach.dapui_config = function()
-  dap_ui.open()
-end
-dap.listeners.before.launch.dapui_config = function()
-  dap_ui.open()
-end
-dap.listeners.before.event_terminated.dapui_config = function()
-  dap_ui.close()
-end
-dap.listeners.before.event_exited.dapui_config = function()
-  dap_ui.close()
-end
+  vim.fn.sign_define(
+    "DapBreakpoint",
+    { text = "●", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+  )
+  vim.fn.sign_define(
+    "DapBreakpointCondition",
+    { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+  )
+  vim.fn.sign_define(
+    "DapBreakpointRejected",
+    { text = "", texthl = "DapBreakpoint", linehl = "DapBreakpoint", numhl = "DapBreakpoint" }
+  )
+  vim.fn.sign_define(
+    "DapLogPoint",
+    { text = "", texthl = "DapLogPoint", linehl = "DapLogPoint", numhl = "DapLogPoint" }
+  )
+  vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped", linehl = "DapStopped", numhl = "DapStopped" })
 
--- Key mappings for DAP
-vim.keymap.set("n", "<F1>", "<Cmd>DapToggleBreakpoint<CR>", { desc = "Toggle breakpoint" })
+  local function set_conditional_breakpoint()
+    vim.ui.input({ prompt = "Enter condition for breakpoint: " }, function(condition)
+      if condition then
+        require("dap").set_breakpoint(condition)
+      end
+    end)
+  end
 
-vim.keymap.set("n", "<F2>", function()
-  set_conditional_breakpoint()
-end, { desc = "Set conditional breakpoint" })
+  local function set_hit_breakpoint()
+    vim.ui.input({ prompt = "Enter hit count for breakpoint: " }, function(hit_condition)
+      if hit_condition then
+        require("dap").set_breakpoint(nil, hit_condition)
+      end
+    end)
+  end
 
-vim.keymap.set("n", "<F3>", function()
-  set_hit_breakpoint()
-end, { desc = "Set hit breakpoint" })
+  local function set_log_breakpoint()
+    vim.ui.input({ prompt = "Enter log message for breakpoint: " }, function(log_message)
+      if log_message then
+        require("dap").set_breakpoint(nil, nil, log_message)
+      end
+    end)
+  end
 
-vim.keymap.set("n", "<F4>", function()
-  set_log_breakpoint()
-end, { desc = "Set log point" })
+  -- Key mappings for DAP
+  vim.keymap.set("n", "<F1>", "<Cmd>DapToggleBreakpoint<CR>", { desc = "Toggle breakpoint" })
 
-vim.keymap.set("n", "<F5>", "<Cmd>DapContinue<CR>", { desc = "Continue debugging session" })
+  vim.keymap.set("n", "<F2>", function()
+    set_conditional_breakpoint()
+  end, { desc = "Set conditional breakpoint" })
+  vim.keymap.set("n", "<F3>", function()
+    set_hit_breakpoint()
+  end, { desc = "Set hit breakpoint" })
 
-vim.keymap.set("n", "<F6>", "<Cmd>DapTerminate<CR>", { desc = "Terminate debugging session" })
+  vim.keymap.set("n", "<F4>", function()
+    set_log_breakpoint()
+  end, { desc = "Set log point" })
 
-vim.keymap.set("n", "<F7>", function()
-  require("dapui").toggle()
-end, { desc = "Toggle the DAP UI" })
+  vim.keymap.set("n", "<F5>", "<Cmd>DapContinue<CR>", { desc = "Continue debugging session" })
 
-vim.keymap.set("n", "<F10>", "<Cmd>DapStepOver<CR>", { desc = "Step over the current line" })
+  vim.keymap.set("n", "<F6>", "<Cmd>DapTerminate<CR>", { desc = "Terminate debugging session" })
 
-vim.keymap.set("n", "<F11>", "<Cmd>DapStepInto<CR>", { desc = "Step into the current line" })
+  vim.keymap.set("n", "<F7>", function()
+    require("dapui").toggle()
+  end, { desc = "Toggle the DAP UI" })
 
-vim.keymap.set("n", "<F12>", "<Cmd>DapStepOut<CR>", { desc = "Step out of the current line" })
+  vim.keymap.set("n", "<F10>", "<Cmd>DapStepOver<CR>", { desc = "Step over the current line" })
 
--- Floating windows for DAP UI elements
-vim.keymap.set("n", "<leader>db", function()
-  require("dapui").float_element("breakpoints", { enter = true, position = "center" })
-end, { desc = "Show breakpoints" })
+  vim.keymap.set("n", "<F11>", "<Cmd>DapStepInto<CR>", { desc = "Step into the current line" })
 
-vim.keymap.set("n", "<leader>ds", function()
-  require("dapui").float_element("stacks", { enter = true, position = "center" })
-end, { desc = "Show stacks" })
+  vim.keymap.set("n", "<F12>", "<Cmd>DapStepOut<CR>", { desc = "Step out of the current line" })
 
-vim.keymap.set("n", "<leader>dv", function()
-  require("dapui").float_element("scopes", { enter = true, position = "center" })
-end, { desc = "Show variables" })
+  -- Floating windows for DAP UI elements
+  vim.keymap.set("n", "<leader>db", function()
+    require("dapui").float_element("breakpoints", { enter = true, position = "center" })
+  end, { desc = "Show breakpoints" })
 
-vim.keymap.set("n", "<leader>dw", function()
-  require("dapui").float_element("watches", { enter = true, position = "center" })
-end, { desc = "Show watches" })
+  vim.keymap.set("n", "<leader>ds", function()
+    require("dapui").float_element("stacks", { enter = true, position = "center" })
+  end, { desc = "Show stacks" })
+
+  vim.keymap.set("n", "<leader>dv", function()
+    require("dapui").float_element("scopes", { enter = true, position = "center" })
+  end, { desc = "Show variables" })
+
+  vim.keymap.set("n", "<leader>dw", function()
+    require("dapui").float_element("watches", { enter = true, position = "center" })
+  end, { desc = "Show watches" })
+end)
--- plugin/neotest.lua
+++ plugin/neotest.lua
@@ -20,37 +20,38 @@ local function is_test_file(file)
   return false
 end
 
-require("nvim-treesitter").install({ "c", "cpp" }):wait()
+vim.schedule(function()
+  require("nvim-treesitter").install({ "c", "cpp" }):wait()
+  require("neotest").setup({
+    adapters = {
+      require("neotest-gtest").setup({ is_test_file = is_test_file }),
+      require("neotest-python"),
+    },
+  })
 
-require("neotest").setup({
-  adapters = {
-    require("neotest-gtest").setup({ is_test_file = is_test_file }),
-    require("neotest-python"),
-  },
-})
-
-vim.keymap.set("n", "<leader>tr", function()
-  require("neotest").run.run({ cwd = vim.fn.expand("%:p:h") })
-end, { desc = "Run nearest test" })
-vim.keymap.set("n", "<leader>tl", function()
-  require("neotest").run.run_last()
-end, { desc = "Run last test" })
-vim.keymap.set("n", "<leader>tf", function()
-  require("neotest").run.run(vim.fn.expand("%"), { cwd = vim.fn.expand("%:p:h") })
-end, { desc = "Run tests in file" })
-vim.keymap.set("n", "<leader>td", function()
-  require("neotest").run.run({ strategy = "dap", cwd = vim.fn.expand("%:p:h") })
-end, { desc = "Debug nearest test" })
-vim.keymap.set("n", "<leader>ts", function()
-  require("neotest").summary.toggle()
-end, { desc = "Toggle test summary" })
-vim.keymap.set("n", "<leader>to", function()
-  require("neotest").output_panel.toggle()
-end, { desc = "Toggle output panel" })
-vim.keymap.set("n", "<leader>tn", function()
-  require("neotest").jump.next({ status = "failed" })
-end, { desc = "Jump to next failed test" })
-vim.keymap.set("n", "<leader>tp", function()
-  require("neotest").jump.prev({ status = "failed" })
-end, { desc = "Jump to previous failed test" })
-vim.keymap.set("n", "<leader>tc", "<Cmd>ConfigureGtest<CR>", { desc = "Configure Google Test" })
+  vim.keymap.set("n", "<leader>tr", function()
+    require("neotest").run.run({ cwd = vim.fn.expand("%:p:h") })
+  end, { desc = "Run nearest test" })
+  vim.keymap.set("n", "<leader>tl", function()
+    require("neotest").run.run_last()
+  end, { desc = "Run last test" })
+  vim.keymap.set("n", "<leader>tf", function()
+    require("neotest").run.run(vim.fn.expand("%"), { cwd = vim.fn.expand("%:p:h") })
+  end, { desc = "Run tests in file" })
+  vim.keymap.set("n", "<leader>td", function()
+    require("neotest").run.run({ strategy = "dap", cwd = vim.fn.expand("%:p:h") })
+  end, { desc = "Debug nearest test" })
+  vim.keymap.set("n", "<leader>ts", function()
+    require("neotest").summary.toggle()
+  end, { desc = "Toggle test summary" })
+  vim.keymap.set("n", "<leader>to", function()
+    require("neotest").output_panel.toggle()
+  end, { desc = "Toggle output panel" })
+  vim.keymap.set("n", "<leader>tn", function()
+    require("neotest").jump.next({ status = "failed" })
+  end, { desc = "Jump to next failed test" })
+  vim.keymap.set("n", "<leader>tp", function()
+    require("neotest").jump.prev({ status = "failed" })
+  end, { desc = "Jump to previous failed test" })
+  vim.keymap.set("n", "<leader>tc", "<Cmd>ConfigureGtest<CR>", { desc = "Configure Google Test" })
+end)
