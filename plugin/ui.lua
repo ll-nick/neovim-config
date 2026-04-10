13be453f3a12e6706ff88bbc37a0b8e58db16ffc
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-10 10:40:57 +0200

    Set highlight group for border color

--- plugin/ui.lua
+++ plugin/ui.lua
@@ -9,3 +9,11 @@ require("tiny-cmdline").setup({
   native_types = {},
   on_reposition = require("tiny-cmdline").adapters.blink,
 })
+
+vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
+  callback = function()
+    local bg = vim.api.nvim_get_hl(0, { name = "MsgArea", link = false }).bg
+    local fg = vim.api.nvim_get_hl(0, { name = "FloatBorder", link = false }).fg
+    vim.api.nvim_set_hl(0, "TinyCmdlineBorder", { fg = fg, bg = bg })
+  end,
+})
