f4ac7b6affac45e2590685d617f67feb403cb0a1
ll-nick <68419636+ll-nick@users.noreply.github.com>
2026-04-17 15:42:10 +0200

    Enable auto_insert in blink.cmp

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -5,8 +5,18 @@ vim.pack.add({
 
 require("blink.cmp").setup({
   completion = {
-    list = { selection = { preselect = false, auto_insert = false } },
-    documentation = { auto_show = true, auto_show_delay_ms = 500 },
-    ghost_text = { enabled = true },
+    list = {
+      selection = {
+        preselect = false,
+        auto_insert = true,
+      },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+    },
+    ghost_text = {
+      enabled = true,
+    },
   },
 })
